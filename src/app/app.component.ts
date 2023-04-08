72acad05106b0fc01e75dc3d96a7c7e39376d186
Christian Knoll <chris@sectorfive.com>
2023-04-08 11:10:51 -0400

    Updates - colors, gif selection, progress bar, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import {
-  fromEvent,
-  interval,
-  merge,
-  Observable,
-  Subscription,
-  switchMap,
-} from 'rxjs';
+import { fromEvent, interval, merge, Subscription, switchMap } from 'rxjs';
 
 import { ThemeService } from './theme.service';
+
+export interface AudioModel {
+  audioTitle: string;
+  audioPath: string;
+}
+
+export interface GifModel {
+  id: number;
+  gifTitle: string;
+  gifPath: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -17,40 +21,121 @@ import { ThemeService } from './theme.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  subscription!: Subscription;
   showMenu = true;
   timerAlmostUp!: boolean;
   duration!: number;
   remainingTime = 0;
+  remainingPercent = 100;
   audioPlaying: boolean = false;
   darkTheme: FormControl;
   showCredits = false;
-  displayError: boolean = false;
-  allEvents: Observable<number> | undefined;
-  myTimer: Subscription | undefined;
+  audioSelections: AudioModel[] = [];
+  audioSelection!: AudioModel;
+  gifSelections!: GifModel[];
+  gifSelection!: GifModel;
 
   constructor(
     private themeService: ThemeService,
     private formBuilder: FormBuilder
   ) {
+    // Set up the theme and CSS Variables
     this.darkTheme = this.formBuilder.control(false);
     this.darkTheme.valueChanges.subscribe((val: boolean) => {
-      if (val) {
-        this.themeService.toggleDark();
-      } else {
-        this.themeService.toggleLight();
-      }
+      val ? this.themeService.toggleDark() : this.themeService.toggleLight();
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.prepareAudio();
+    this.prepareGif();
+  }
 
-  startTimer(duration: string) {
-    const regex = /^\d+$/;
-    if (!regex.test(duration)) {
-      this.displayError = true;
+  prepareAudio(): void {
+    // Name and audio source for any sounds you want to play for the alert
+    this.audioSelections = [
+      {
+        audioTitle: 'Birds Chirping',
+        audioPath: 'https://freewavesamples.com/files/Chirping-Birds-2.wav',
+      },
+      {
+        audioTitle: 'Drum Beat',
+        audioPath: 'https://freewavesamples.com/files/Casio-MT-45-16-Beat.wav',
+      },
+      {
+        audioTitle: 'Cheesy Bass Rhythm',
+        audioPath:
+          'https://freewavesamples.com/files/Ensoniq-ZR-76-01-Dope-77.wav',
+      },
+      {
+        audioTitle: 'No Audio (Silent)',
+        audioPath: '',
+      },
+    ];
+    // Use the first option as the default
+    this.audioSelection = this.audioSelections[0];
+  }
+
+  prepareGif(): void {
+    // Animated gif to display for the alert
+    this.gifSelections = [
+      {
+        id: 0,
+        gifTitle: 'Bunny Gong',
+        gifPath:
+          'https://gifdb.com/images/high/bunny-wake-up-gong-ywvit3j8auv8abgu.webp',
+      },
+      {
+        id: 1,
+        gifTitle: 'Vaporwave Mountain',
+        gifPath:
+          'https://gifdb.com/images/file/vaporwave-mountain-peak-moon-1yg3i0ccfu1bkli1.gif',
+      },
+      {
+        id: 2,
+        gifTitle: 'Vaporwave Dancer',
+        gifPath:
+          'https://gifdb.com/images/high/vaporwave-model-dancing-zzootpk2etvlmknu.webp',
+      },
+      {
+        id: 3,
+        gifTitle: 'Vaporwave 80s',
+        gifPath:
+          'https://gifdb.com/images/high/vaporwave-80s-city-mxa15mtookmrjlyk.webp',
+      },
+      {
+        id: 4,
+        gifTitle: 'Vaporwave Checker',
+        gifPath:
+          'https://gifdb.com/images/file/vaporwave-checkered-pyramid-yi9jnegowvbjsshz.gif',
+      },
+    ];
+    // Use the first option as the default
+    this.gifSelection = this.gifSelections[0];
+  }
+
+  onSelectAudio(value: string): void {
+    const audioSelection = this.audioSelections.find(
+      (a: { audioTitle: string }) => a.audioTitle == value
+    );
+    if (audioSelection) {
+      this.audioSelection = audioSelection;
+    }
+  }
+
+  playAudio(): void {
+    if (this.audioPlaying) {
       return;
     }
-    this.displayError = false;
+    const audio = new Audio(this.audioSelection.audioPath);
+    audio.play();
+  }
+
+  onSelectGif(gifIdx: number): void {
+    this.gifSelection = this.gifSelections[gifIdx];
+  }
+
+  startTimer(duration: string) {
     this.showMenu = false;
     this.duration = parseInt(duration) * 60;
     const click$ = fromEvent<MouseEvent>(document, 'click');
@@ -58,17 +143,20 @@ export class AppComponent implements OnInit {
     const keyPress$ = fromEvent<KeyboardEvent>(document, 'keyup');
     const allEvents = merge(click$, keyPress$, mouseMove$);
 
-    this.allEvents = allEvents.pipe(
+    const result = allEvents.pipe(
       switchMap(() => {
+        this.remainingPercent = 100;
         this.remainingTime = this.duration;
         this.timerAlmostUp = false;
         // tick every 1s
         return interval(1000);
       })
     );
-    this.myTimer = this.allEvents.subscribe((x) => {
-      console.log(`X is ${x}`);
-      if (x >= this.duration - 10) {
+
+    this.subscription && this.subscription.unsubscribe();
+
+    this.subscription = result.subscribe((x) => {
+      if (x >= this.duration - 55) {
         this.playAudio();
         this.audioPlaying = true;
         this.timerAlmostUp = true;
@@ -77,29 +165,26 @@ export class AppComponent implements OnInit {
       }
       if (this.remainingTime >= 0) {
         this.remainingTime -= 1;
+      } else {
+        this.remainingPercent = 100;
       }
-      console.log('remaining time is', this.remainingTime);
+      this.remainingPercent = Math.round(
+        (this.remainingTime / this.duration) * 100
+      );
+      console.log('remainingTime / duration * 100 is', this.remainingPercent);
     });
-  }
-
-  playAudio() {
-    if (this.audioPlaying) {
-      return;
-    }
-    const audio = new Audio(
-      'https://freewavesamples.com/files/Ensoniq-ZR-76-01-Dope-77.wav'
-    );
-    audio.play();
   }
 
   timerToggle(): void {
     this.timerAlmostUp = !this.timerAlmostUp;
   }
-  restartTimer(): void {
+  showMenuToggle(): void {
+    this.subscription.unsubscribe();
     this.showMenu = !this.showMenu;
-    this.myTimer?.unsubscribe();
   }
   showCreditsToggle(): void {
     this.showCredits = !this.showCredits;
   }
 }
+
+// https://forums.eveonline.com/t/hacs-for-level-4-missions/346610/7
--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,20 +1,28 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 
-export const darkTheme = {
-  'primary-color': '#455363',
-  'background-color': '#1f2935',
-  'text-color': '#fff',
-  'credits-background': '#161e27',
-  'border-color': '#1e3652',
-};
-
 export const lightTheme = {
-  'primary-color': '#fff',
+  'text-primary': '#a370f2',
+  'text-secondary': '#d95fcc',
+  'text-tertiary': '#4c0be3',
+  'card-bkd': '#eee',
   'background-color': '#e5e5e5',
   'text-color': '#2d2d2d',
   'credits-background': '#ddd',
   'border-color': '#bbb',
+  'shadow-color': '#ccc',
+};
+
+export const darkTheme = {
+  'text-primary': '#73eff0',
+  'text-secondary': '#eb21b6',
+  'text-tertiary': '#32a1c6',
+  'card-bkd': '#eee',
+  'background-color': '#1f2935',
+  'text-color': '#fff',
+  'credits-background': '#161e27',
+  'border-color': '#1e3652',
+  'shadow-color': '#222',
 };
 
 @Injectable()
@@ -29,8 +37,9 @@ export class ThemeService {
     this.setTheme(lightTheme);
   }
 
-  private setTheme(theme: any) {
-    console.log('theme', theme);
+  // The main change I made was to specify the type of the theme parameter as a Record<string, string>. This tells TypeScript that theme is an object with string keys and string values. By doing this, TypeScript will no longer raise the TS2564 error which is related to using an empty object ({}) as a type annotation.
+  // I also used the Record utility type instead of an empty object ({}) to provide a more explicit and descriptive type annotation for the theme parameter. This makes it easier for other developers to understand what the expected shape of the theme object should be.
+  private setTheme(theme: Record<string, string>) {
     Object.keys(theme).forEach((k) =>
       this.document.documentElement.style.setProperty(`--${k}`, theme[k])
     );
